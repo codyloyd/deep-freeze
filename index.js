3a63a95ba02a2f95893b2f37edcfe99c024c7b7e
Cody Loyd <codyloyd@gmail.com>
2017-03-02 17:12:15 -0600

    add support for multiple arguments

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
-module.exports = function deepFreeze (o) {
-  Object.freeze(o);
+module.exports = function deepFreeze () {
+  var args = [].slice.call(arguments);
+  var freeze = function(o) {
+      Object.freeze(o);
 
-  Object.getOwnPropertyNames(o).forEach(function (prop) {
-    if (o.hasOwnProperty(prop)
-    && o[prop] !== null
-    && (typeof o[prop] === "object" || typeof o[prop] === "function")
-    && !Object.isFrozen(o[prop])) {
-      deepFreeze(o[prop]);
-    }
-  });
+      Object.getOwnPropertyNames(o).forEach(function (prop) {
+        if (o.hasOwnProperty(prop)
+        && o[prop] !== null
+        && (typeof o[prop] === "object" || typeof o[prop] === "function")
+        && !Object.isFrozen(o[prop])) {
+          deepFreeze(o[prop]);
+        }
+      });
+    
+    return o;
+  }
   
-  return o;
+  for (var index = 0; index < args.length; index++) {
+    freeze(args[index])
+  }
 };
--- test/deep.js
+++ test/deep.js
@@ -11,3 +11,14 @@ test('deep freeze', function (t) {
     Buffer.prototype.z = 3;
     t.equal(Buffer.prototype.z, undefined);
 });
+
+test('multiple arguments', function (t) {
+    t.plan(2);
+    var array1 = [0]
+    var array2 = [0]
+    deepFreeze(array1,array2)
+    array1[0] = 1
+    array2[0] = 1
+    t.equal(array1[0], 0)
+    t.equal(array2[0], 0)
+});
